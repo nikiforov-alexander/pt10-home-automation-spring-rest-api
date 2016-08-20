b9e7e54679662bb423873ff22e466d2d914b7141
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-08-20 13:08:32 +0200

    changed so that it runs. added getters, setters, default constructor and needed annotations

--- src/main/java/com/teamtreehouse/home/model/Room.java
+++ src/main/java/com/teamtreehouse/home/model/Room.java
@@ -1,6 +1,51 @@
 package com.teamtreehouse.home.model;
 
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
 public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
+    @NotEmpty
     private String name;
+
+    private int squareFootage;
+
+    //
+    // Getters and Setters
+    //
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSquareFootage() {
+        return squareFootage;
+    }
+
+    public void setSquareFootage(int squareFootage) {
+        this.squareFootage = squareFootage;
+    }
+
+    public Room() {
+        // default constructor for JPA
+    }
 }
