957c53ea93471284c6abca346bb378a859415754
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-08-21 21:09:12 +0200

    without `SpringBootApplication` that was used by Craig. Now it works in "naked" way: no RESTs created for us

--- src/main/java/com/teamtreehouse/home/Application.java
+++ src/main/java/com/teamtreehouse/home/Application.java
@@ -1,9 +1,11 @@
 package com.teamtreehouse.home;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
