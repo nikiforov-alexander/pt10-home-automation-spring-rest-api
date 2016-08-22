39404aca15f15efa7bbd0451d753cf248b91e34f
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2016-08-22 14:55:59 +0200

    changed return types of methods, implemented `findAll` and `findOne` returning Room and List<Room>

--- src/main/java/com/teamtreehouse/home/service/RoomService.java
+++ src/main/java/com/teamtreehouse/home/service/RoomService.java
@@ -2,6 +2,9 @@ package com.teamtreehouse.home.service;
 
 import com.teamtreehouse.home.model.Room;
 
+import java.util.List;
+
 public interface RoomService {
-    Iterable<Room> findAll();
+    List<Room> findAll();
+    Room findOne(Long id);
 }
--- src/main/java/com/teamtreehouse/home/service/RoomServiceImpl.java
+++ src/main/java/com/teamtreehouse/home/service/RoomServiceImpl.java
@@ -1,9 +1,13 @@
 package com.teamtreehouse.home.service;
 
+import com.google.common.collect.Lists;
 import com.teamtreehouse.home.dao.RoomDao;
 import com.teamtreehouse.home.model.Room;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class RoomServiceImpl implements RoomService{
@@ -11,7 +15,13 @@ public class RoomServiceImpl implements RoomService{
     private RoomDao roomDao;
 
     @Override
-    public Iterable<Room> findAll() {
-        return roomDao.findAll();
+    public List<Room> findAll() {
+        // converts Iterable from CRUD to List<Room> using Guava google
+        return Lists.newArrayList(roomDao.findAll());
+    }
+
+    @Override
+    public Room findOne(Long id) {
+        return roomDao.findOne(id);
     }
 }
