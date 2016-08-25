0cd4c4edaf8a42830fde71b4bc025fc29bee6e59
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-08-25 21:55:56 +0200

    set up User class for now just like in workshop

--- src/main/java/com/teamtreehouse/home/model/User.java
+++ src/main/java/com/teamtreehouse/home/model/User.java
@@ -1,4 +1,81 @@
 package com.teamtreehouse.home.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.persistence.Entity;
+
+@Entity
 public class User {
+    public static final PasswordEncoder PASSWORD_ENCODER =
+            new BCryptPasswordEncoder();
+    private String firstName;
+    private String lastName;
+    private String username;
+
+    @JsonIgnore
+    private String password;
+    @JsonIgnore
+    private String[] roles;
+
+    // default constructor for JPA
+    // and calling BaseEntity constructor
+    protected User() {
+        super();
+    }
+
+    public User(
+            String firstName,
+            String lastName,
+            String username,
+            String password,
+            String[] roles) {
+        this();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.roles = roles;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String[] getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String[] roles) {
+        this.roles = roles;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = PASSWORD_ENCODER.encode(password);
+    }
 }
