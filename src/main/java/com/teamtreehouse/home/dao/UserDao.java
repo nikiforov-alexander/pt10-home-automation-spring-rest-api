5a7e844afd4279d3f9aab8423770daab61e2db34
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2016-08-26 15:55:54 +0200

    made `UserDao` extend CrudRepository, because it is not REST exposed, added comments

--- src/main/java/com/teamtreehouse/home/dao/UserDao.java
+++ src/main/java/com/teamtreehouse/home/dao/UserDao.java
@@ -1,10 +1,12 @@
 package com.teamtreehouse.home.dao;
 
 import com.teamtreehouse.home.model.User;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+// make sure that this directory is not exposed to REST API
 @RepositoryRestResource(exported = false)
-public interface UserDao extends PagingAndSortingRepository<User, Long>{
+public interface UserDao extends CrudRepository<User, Long> {
+    // "query" method of SpringData: finds user by username
     User findByUsername(String username);
 }
