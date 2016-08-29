2f5d254681d71af66e0b7f26bad4c519be177370
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-08-29 14:40:28 +0200

    added method setting up `authentication` as I thought, taken from Spring Security Workshop. But It didn't work: later this is subject to be removed

--- src/main/java/com/teamtreehouse/home/config/WebSecurityConfiguration.java
+++ src/main/java/com/teamtreehouse/home/config/WebSecurityConfiguration.java
@@ -1,14 +1,20 @@
 package com.teamtreehouse.home.config;
 
+import com.teamtreehouse.home.CustomUserDetailsService;
 import com.teamtreehouse.home.model.User;
-import com.teamtreehouse.home.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.repository.query.spi.EvaluationContextExtension;
+import org.springframework.data.repository.query.spi.EvaluationContextExtensionSupport;
+import org.springframework.security.access.expression.SecurityExpressionRoot;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @Configuration
 @EnableWebSecurity
@@ -32,5 +38,23 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .httpBasic()
                 .and()
                 .csrf().disable();
+    }
+
+    // I added this because I thought it will help to resolve
+    // 'authentication' in @PreAuthorize annotations. But this has no
+    // power. I will leave it for now. Later is subject to removal
+    @Bean
+    public EvaluationContextExtension securityExtension() {
+        return new EvaluationContextExtensionSupport() {
+            @Override
+            public String getExtensionId() {
+                return "security";
+            }
+            @Override
+            public Object getRootObject() {
+                Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+                return new SecurityExpressionRoot(authentication) {};
+            }
+        };
     }
 }
