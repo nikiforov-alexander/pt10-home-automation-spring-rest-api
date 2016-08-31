3090e7a128ecc769f19ca2a0da9426de232c417f
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-08-31 18:10:45 +0200

    added RoomDaoTest file with first unit test: that creating room with user admin indeed creates room

---
+++ src/test/java/com/teamtreehouse/home/dao/RoomDaoTest.java
@@ -0,0 +1,73 @@
+package com.teamtreehouse.home.dao;
+
+import com.teamtreehouse.home.Application;
+import com.teamtreehouse.home.model.Room;
+import com.teamtreehouse.home.service.CustomUserDetailsService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+// run integration test because that is the only way
+// to test Spring Data Reps
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(Application.class)
+@WebIntegrationTest
+public class RoomDaoTest {
+    // autowire roomDao
+    @Autowired
+    private RoomDao roomDao;
+
+    // autowire userDetailsService to load user
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
+    // setting port number to 8081, so that we
+    // do not interfere with real bootRun
+    static {
+        System.setProperty("server.port", "8081");
+    }
+
+    // login user depending on username
+    // can be "sa" with ROLE_USER, ROLE_ADMIN
+    // or "jd" with ROLE_USER
+    private void setUpUserByUsername(String username) {
+        UserDetails user =
+                customUserDetailsService.loadUserByUsername(username);
+        SecurityContextHolder.getContext().setAuthentication(
+                new UsernamePasswordAuthenticationToken(
+                        user , null, user.getAuthorities()
+                )
+        );
+    }
+
+    @Test
+    public void userWithAdminRoleCanCreateRoom() throws Exception {
+        // Arrange:
+        // login admin user
+        setUpUserByUsername("sa");
+        // create test room
+        Room room = new Room("test room", 1);
+
+        // Act and Assert:
+        // When test room is saved with admin user
+        Room savedRoom = roomDao.save(room);
+
+        // Then some room can be found by id in
+        // roomDao
+        assertThat(
+                roomDao.findOne(
+                        savedRoom.getId()
+                ),
+                notNullValue(Room.class)
+        );
+    }
+}
