e9fe0100d5180bfba9fa5f69cdf66e0002f3d8d0
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-09-01 21:45:47 +0200

    @Override `save` Device method, so that only ROLE_ADMIN or room admins

--- src/main/java/com/teamtreehouse/home/dao/DeviceDao.java
+++ src/main/java/com/teamtreehouse/home/dao/DeviceDao.java
@@ -1,11 +1,13 @@
 package com.teamtreehouse.home.dao;
 
 import com.teamtreehouse.home.model.Device;
+import com.teamtreehouse.home.model.Room;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -18,4 +20,12 @@ public interface DeviceDao
     Page<Device> findByNameContaining(
             @Param("name") String name,
             Pageable pageable);
+
+    // here i ensure that only if user is in
+    // room.administrators OR he is administrator
+    // he can save/modify device
+    @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN') or " +
+            "#device.room.hasAdministrator(authentication.principal)")
+    Device save(@Param("device") Device device);
 }
